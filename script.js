63341fc58accae335aec47c99df244fd0bbd6ca1
Hea092024 <Hea092024@gmail.com>
2025-01-15 13:31:14 +0100

    .

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ let surname = "Acevedo"
 let age = 41
 
 console.log(age);
-age = 41
-console.log(age);
+age = 41 // dont need let here since its already been decalared
+console.log(age); //output will be the same as above
 
 console.log (name);
 console.log(surname);
