301f3e1809af8300af064c99101535b08ef9976d
Hea092024 <Hea092024@gmail.com>
2025-01-15 13:33:54 +0100

    .

--- script.js
+++ script.js
@@ -9,3 +9,8 @@ console.log(age); //output will be the same as above
 console.log (name);
 console.log(surname);
 console.log(name + " " + surname);
+
+const pi = 3.14;
+pi =10;
+console.log(pi); // it will give an error, because pi is a constant
+
